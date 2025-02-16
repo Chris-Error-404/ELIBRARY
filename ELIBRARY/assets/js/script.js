861db058dd262ceb14c1d45943839514ca81efad
Nzenwata Christopher <nzenwatachristopher186@gmail.com>
2025-02-16 23:43:07 +0100

    update

--- ELIBRARY/assets/js/script.js
+++ ELIBRARY/assets/js/script.js
@@ -40,3 +40,28 @@ listItems.forEach(item => {
         }
     });
 });
+
+
+
+//slider
+let index = 0;
+const slides = document.querySelector('.slides');
+
+function showSlide() {
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+function nextSlide() {
+    index = (index + 1) % 3;
+    showSlide();
+}
+
+// Auto-slide every 3 seconds
+setInterval(nextSlide, 3000);
+
+// Allow manual navigation
+function prevSlide() {
+    index = (index - 1 + 3) % 3;
+    showSlide();
+}
+
